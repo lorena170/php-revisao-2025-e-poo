0e5cd0ca1a39244f97a80ab52979e91c61430ad4
lorena170 <lorena.c.souza6@aluno.senai.br>
2025-04-22 11:38:41 -0300

    ex-concluido

--- PHP (Rev) + POO_Alunos/4_mini-exercicios_alunos/ex_1_solucao.php
+++ PHP (Rev) + POO_Alunos/4_mini-exercicios_alunos/ex_1_solucao.php
@@ -1,1 +1,111 @@
-<!-- 1ª Digitação (Aqui) -->
+<?php
+
+// Classe abstrata ItemBiblioteca
+abstract class ItemBiblioteca {
+    protected $titulo;
+    protected $codigo;
+    protected $disponivel;
+
+    public function __construct($titulo, $codigo) {
+        $this->titulo = $titulo;
+        $this->codigo = $codigo;
+        $this->disponivel = true;
+    }
+
+    // Método abstrato para calcular multa
+    abstract public function calcular_multa($dias_atraso);
+
+    // Método para emprestar o item
+    public function emprestar() {
+        if ($this->disponivel) {
+            $this->disponivel = false;
+            echo get_class($this) . " '{$this->titulo}' emprestado com sucesso!\n";
+        } else {
+            echo get_class($this) . " '{$this->titulo}' não está disponível para empréstimo.\n";
+        }
+    }
+
+    // Método para devolver o item
+    public function devolver() {
+        $this->disponivel = true;
+        echo get_class($this) . " '{$this->titulo}' devolvido com sucesso!\n";
+    }
+
+    // Método para verificar se o item está disponível
+    public function esta_disponivel() {
+        return $this->disponivel;
+    }
+
+    // Método para retornar o título do item
+    public function titulo() {
+        return $this->titulo;
+    }
+}
+
+// Classe Livro que herda de ItemBiblioteca
+class Livro extends ItemBiblioteca {
+    public function calcular_multa($dias_atraso) {
+        return 0.50 * $dias_atraso;
+    }
+}
+
+// Classe Revista que herda de ItemBiblioteca
+class Revista extends ItemBiblioteca {
+    public function calcular_multa($dias_atraso) {
+        return 0.25 * $dias_atraso;
+    }
+}
+
+// Classe Biblioteca para gerenciar os itens
+class Biblioteca {
+    private $itens = [];
+
+    // Adiciona um item ao acervo da biblioteca
+    public function adicionar_item(ItemBiblioteca $item) {
+        $this->itens[$item->titulo()] = $item;
+        echo "Item '{$item->titulo()}' adicionado ao acervo.\n";
+    }
+
+    // Empresta um item baseado no título
+    public function emprestar_item($titulo) {
+        if (isset($this->itens[$titulo])) {
+            $this->itens[$titulo]->emprestar();
+        } else {
+            echo "Item '{$titulo}' não encontrado no acervo.\n";
+        }
+    }
+
+    // Devolve um item baseado no título
+    public function devolver_item($titulo) {
+        if (isset($this->itens[$titulo])) {
+            $this->itens[$titulo]->devolver();
+        } else {
+            echo "Item '{$titulo}' não encontrado no acervo.\n";
+        }
+    }
+}
+
+// Teste do sistema
+$biblioteca = new Biblioteca();
+
+// Criação de itens (Livro e Revista)
+$livro = new Livro("Python para Iniciantes", "L001");
+$revista = new Revista("TechNews", "R001");
+
+// Adicionando itens à biblioteca
+$biblioteca->adicionar_item($livro);
+$biblioteca->adicionar_item($revista);
+
+// Realizando o empréstimo dos itens
+$biblioteca->emprestar_item("Python para Iniciantes");
+$biblioteca->emprestar_item("TechNews");
+
+// Devolvendo o livro
+$biblioteca->devolver_item("Python para Iniciantes");
+
+// Calculando e exibindo as multas
+$dias_atraso = 5;
+echo "\nMulta do livro (5 dias): R$" . number_format($livro->calcular_multa($dias_atraso), 2, ',', '.') . "\n";
+echo "Multa da revista (5 dias): R$" . number_format($revista->calcular_multa($dias_atraso), 2, ',', '.') . "\n";
+
+?>
--- PHP (Rev) + POO_Alunos/4_mini-exercicios_alunos/ex_2_solucao.php
+++ PHP (Rev) + POO_Alunos/4_mini-exercicios_alunos/ex_2_solucao.php
@@ -1,1 +1,111 @@
-<!-- 1ª Digitação (Aqui) -->
+<?php
+
+// Classe abstrata Veiculo
+abstract class Veiculo {
+    protected $modelo;
+    protected $placa;
+    protected $disponivel;
+
+    public function __construct($modelo, $placa) {
+        $this->modelo = $modelo;
+        $this->placa = $placa;
+        $this->disponivel = true;
+    }
+
+    // Método abstrato para calcular o aluguel
+    abstract public function calcularAluguel($dias);
+
+    // Verifica se o veículo está disponível para locação
+    public function isDisponivel() {
+        return $this->disponivel;
+    }
+
+    // Retorna o modelo do veículo
+    public function getModelo() {
+        return $this->modelo;
+    }
+
+    // Realiza o aluguel do veículo
+    public function alugar() {
+        if ($this->disponivel) {
+            $this->disponivel = false;
+            echo get_class($this) . " '{$this->modelo}' alugado com sucesso!\n";
+        } else {
+            echo get_class($this) . " '{$this->modelo}' não está disponível para aluguel.\n";
+        }
+    }
+
+    // Realiza a devolução do veículo
+    public function devolver() {
+        $this->disponivel = true;
+        echo get_class($this) . " '{$this->modelo}' devolvido com sucesso!\n";
+    }
+}
+
+// Classe Carro que herda de Veiculo
+class Carro extends Veiculo {
+    public function calcularAluguel($dias) {
+        return 100.00 * $dias; // R$100 por dia
+    }
+}
+
+// Classe Moto que herda de Veiculo
+class Moto extends Veiculo {
+    public function calcularAluguel($dias) {
+        return 50.00 * $dias; // R$50 por dia
+    }
+}
+
+// Classe Locadora que gerencia os veículos
+class Locadora {
+    private $veiculos = [];
+
+    // Adiciona um veículo à locadora
+    public function adicionarVeiculo(Veiculo $veiculo) {
+        $this->veiculos[$veiculo->getModelo()] = $veiculo;
+        echo "Veículo '{$veiculo->getModelo()}' adicionado ao acervo.\n";
+    }
+
+    // Aluga um veículo baseado no modelo
+    public function alugarVeiculo($modelo) {
+        if (isset($this->veiculos[$modelo])) {
+            $this->veiculos[$modelo]->alugar();
+        } else {
+            echo "Veículo '{$modelo}' não encontrado na locadora.\n";
+        }
+    }
+
+    // Devolve um veículo baseado no modelo
+    public function devolverVeiculo($modelo) {
+        if (isset($this->veiculos[$modelo])) {
+            $this->veiculos[$modelo]->devolver();
+        } else {
+            echo "Veículo '{$modelo}' não encontrado na locadora.\n";
+        }
+    }
+}
+
+// Teste do sistema
+$locadora = new Locadora();
+
+// Criação de veículos (Carro e Moto)
+$carro = new Carro("HB20", "ABC-1234");
+$moto = new Moto("Yamaha XTZ", "XYZ-5678");
+
+// Adicionando veículos à locadora
+$locadora->adicionarVeiculo($carro);
+$locadora->adicionarVeiculo($moto);
+
+// Realizando o aluguel dos veículos
+$locadora->alugarVeiculo("HB20");
+$locadora->alugarVeiculo("Yamaha XTZ");
+
+// Devolvendo o carro
+$locadora->devolverVeiculo("HB20");
+
+// Calculando e exibindo o valor do aluguel por 3 dias
+$dias = 3;
+echo "\nValor do aluguel do carro por {$dias} dias: R$" . number_format($carro->calcularAluguel($dias), 2, ',', '.') . "\n";
+echo "Valor do aluguel da moto por {$dias} dias: R$" . number_format($moto->calcularAluguel($dias), 2, ',', '.') . "\n";
+
+?>
